effaf8a6691bd882e22f6e898f26f4c09d1e3522
EmilioOcelotl <emilio.ocelotl@gmail.com>
2023-10-25 17:08:43 -0600

    orden cronológico

--- src/index.js
+++ src/index.js
@@ -12,8 +12,6 @@ import { Post } from '../js/avLib/Post.js';
 import { DbReader, dbParser, createDoc } from '../js/avLib/dbSetup2'; 
 import { OrbitControls } from '../static/jsm/controls/OrbitControls.js';
 import { TransformControls } from '../static/jsm/controls/TransformControls.js';
-
-const par = new EditorParser();     
     
 const print = document.getElementById('print');
 print.addEventListener('click', printPDF );
@@ -42,6 +40,9 @@ let infoBool = false;
 const disposeButton = document.getElementById("delete");
 disposeButton.addEventListener('click', disposeLines); 
 
+let notas = []; 
+
+const par = new EditorParser();     
 const a = new AudioSetup(); 
 const th = new VideoSetup(); 
 const hy = new HydraTex();
@@ -63,7 +64,7 @@ let materialCurve = new THREE.LineBasicMaterial();
 let curveObject = new THREE.Line(); 
 let curve1 = new THREE.CatmullRomCurve3(); 
 
-const geometryP1 = new THREE.SphereGeometry( 1, 32, 16 ); 
+const geometryP1 = new THREE.SphereGeometry( 0.5, 32, 16 ); 
 const materialP1 = new THREE.MeshBasicMaterial( { color:  0x05ffa1 } ); 
 const sphereP1 = new THREE.Mesh( geometryP1, materialP1 );
 
@@ -108,9 +109,6 @@ const mouse = [.5, .5]
 // const rTarget = new RTarget(); 
 // rTarget.setText(); 
 
-//const db = new DbReader();
-// db.read("./sql/document.db");
-
 let cosa, cosa2;
 let boolCosa; 
 
@@ -126,9 +124,8 @@ document.getElementById("container").onclick = change;
 //const pdfButton = document.getElementById('pdf');
 //pdfButton.addEventListener('click', printPDF );
 
-// extras intervención oci
-
-const meshes = [],materials = [],xgrid = 14, ygrid = 14;
+// Importante: resolver el problema de la iteración doble y la cantidad total de notas
+const meshes = [],materials = [],xgrid = 11, ygrid = 11;
 let material, mesh;
 let an;
 
@@ -176,7 +173,6 @@ let fBool = false;
 
 init(); // los elementos particulares de este init podrían ir en otro lado. En todo caso podría delimitar la escena que antes se detonaba con esta función.     
 function init(){
-
     loadFont();
     
     a.initAudio();
@@ -208,11 +204,11 @@ function init(){
     light2.position.set( 0, 0, 50 );
     th.scene.add( light2 );
     
-    th.renderer2.outputColorSpace = THREE.LinearSRGBColorSpace;
-    // th.renderer2.toneMapping = THREE.ReinhardToneMapping;
-    // th.renderer2.toneMappingExposure = Math.pow( 0.6, 1.5 )
+    // th.renderer2.outputColorSpace = THREE.LinearSRGBColorSpace;
+    //th.renderer2.toneMapping = THREE.ReinhardToneMapping;
+    //th.renderer2.toneMappingExposure = Math.pow( 0.6, 1.5 )
     
-    // un = new UnrealBloom(th.scene, th.camera, th.renderer2); 
+    //un = new UnrealBloom(th.scene, th.camera, th.renderer2); 
     // retro = new Feedback(th.scene, th.renderer2, 1080);
     const geometry44 = new THREE.BoxGeometry( 70, 70, 70 ); 
     const material44 = new THREE.MeshStandardMaterial( { color: 0xffffff, map: renderTarget.texture, roughness: 0.6 } ); 
@@ -250,7 +246,6 @@ function init(){
 
     const xsize = 200 / xgrid;
     const ysize = 200 / ygrid;
-
 
     controls = new OrbitControls( th.camera, th.renderer2.domElement );
     controls.listenToKeyEvents( window ); // optional
@@ -315,17 +310,15 @@ function animate(){
     
     if(lcbool == true){
 
-	//geometryCurve.geometry.verticesNeedUpdate = true;
-	// geometryCurve.needsUpdate = true;
-	// curveObject.geometry.needsUpdate = true;
-
 	let cC = 0;
+
+	// Revisar si estas modificaciones son relevantes
 	
 	for(let i = 0; i < xgrid; i++){
 	    for (let j = 0; j < ygrid; j++){
 		//cubos2[cC].position.x = 1+(pX[cC]* (Math.sin(time2+i)* 2));
 		//cubos2[cC].position.y = 1+(pY[cC]* (Math.sin(time2+j)* 1));
-
+		
 		// podría haber una condicional para la distribución vertical u horizontal
 		//cubos2[cC].position.x = (pX[cC] * (Math.sin(time1+i+j)* 2));
 		//cubos2[cC].position.y = (pY[cC] * (Math.sin(time1+i+j)* 2));
@@ -333,10 +326,10 @@ function animate(){
 
 		// cubos2[cC].rotation.x += Math.sin(time2+i)*0.002; 
 		cubos2[cC].scale.x = 1+Math.sin(time2+i+j)*4;
-		cubos2[cC].scale.x = 1+Math.sin(time2+i+j)*4;
+		cubos2[cC].scale.y = 1+Math.sin(time2+i+j)*4;
 
 		// cubos2[cC].lookAt(0, 0, 0); 
-		// cubos2[cC].lookAt(th.camera.position); 
+		cubos2[cC].lookAt(th.camera.position); 
 		cC++; 
 	    }
 	}
@@ -347,29 +340,13 @@ function animate(){
     th.camera.position.y += ( - mouseY - th.camera.position.y ) * .5;
     th.camera.lookAt( th.scene.position );
     */
-    
+
+    // Esto se tiene que convertir en otra cosa
     if(boolCosa){
-	// la función map aquí no funciona jaja
-	// parece que no funciona dinámicamente, solo una vez, al inicio. 
 	cosa.pointer = cursorX / 20;
-	//cosa2.pointer = cursorY /20; 
-	//cosa.pointer = map_range(cursorX, 0, 1920, 0, 1);
-	//cosa2.pointer = map_range(cursorY, 0, 1920, 0, 1); 
 	cosa.freqScale =  (cursorY/100)-2.2;
-	//cosa2.freqScale = (cursorY/100)-2.2*2;
-	// cosa.freqScale = map_range(cursorY, 0, 1080, 0.5, 4);
-	// console.log((cursorY/200)-2.2); 
     }    
 
-    //let interStr = ''; 
-    // find intersections
-
-    /*
-    for(let i = 0; i < menuC1str.length; i++){
-	cubos[i].scale.x = Math.sin(((time2*0.1)+i*2))*0.5; 
-	}
-    */
-    
     raycaster.setFromCamera( pointer, th.camera );
     const intersects = raycaster.intersectObjects( th.scene.children, true );
 
@@ -387,12 +364,6 @@ function animate(){
 	    document.getElementById("container").style.cursor = "pointer";
 	    interStr = INTERSECTED.userdata.id;
 	    infoBool = false; 
-	    // aqui va el mensaje
-	    
-	    //console.log(notas[parseInt(INTERSECTED.userdata.id)]); 
-	    // texto(notas[parseInt(INTERSECTED.userdata.id)]); // fuentes no funciona asi  
-	    
-	    // Parece ser que calcular la geometría sigue siendo demasiado costosa, tal vez sea necesario guardar en módulos más pequeños. 
 
 	    if( fBool){
 		onclick=function(){
@@ -402,10 +373,8 @@ function animate(){
 		    var markd = markdown[parseInt(INTERSECTED.userdata.id.slice(0, 4))];  
 		    texto(markd);
 		    controls.target =INTERSECTED.position; 
-		    // console.log(INTERSECTED);
 		    if(lcbool){
 			positions.push(INTERSECTED.position);
-			console.log(positions);
 			curve(positions); 
 		    }
 		}; 
@@ -433,7 +402,7 @@ function animate(){
     TWEEN.update();
    
     hy.vit.needsUpdate = true; 
-    const delta = clock.getDelta();
+    //const delta = clock.getDelta();
 
     renderTarget.flipY = true;
     renderTarget.needsUpdate = true;
@@ -444,7 +413,7 @@ function animate(){
     th.renderer2.setRenderTarget(null);
     
     th.renderer2.render( th.scene, th.camera );
-    // un.render2(delta);
+    //un.render2(delta);
     requestAnimationFrame( animate );
 
 }
@@ -458,15 +427,13 @@ function change(){
     }
  
     if(interStr == 'iniciar'){
-
 	saveNotes(); 
-	
 	const coords = {x: th.camera.position.x,
 			y: th.camera.position.y,
 			z: th.camera.position.z} // Start at (0, 0)
 	
 	tween = new TWEEN.Tween(coords, false) // Create a new tween that modifies 'coords'.
-	    .to({x: 0, y: 0, z: 65}, 2000) // Move to (300, 200) in 1 second.
+	    .to({x: 0, y: 0, z: 30}, 2000) // Move to (300, 200) in 1 second.
 	    .easing(TWEEN.Easing.Quadratic.InOut) 
 	    .onUpdate(() => {
 		th.camera.position.z=coords.z;
@@ -481,10 +448,8 @@ function change(){
 		request.open('GET', 'snd/uxmal.wav', true);
 		request.responseType = 'arraybuffer';
 		self.buffer = 0; 
-		// console.log(this.request.response); 
 		request.onload = function() {
 		    let audioData = request.response;
-		    // console.log(audioData); 
 		    a.audioCtx.decodeAudioData(audioData, function(buffer) {
 			// buffer = buffer2;
 			boolCosa = true; 
@@ -513,30 +478,6 @@ function change(){
 	    .start() // Start the tween immediately. No poner alguna propiedad, supongo que sustituye el tiempo de inicio y llegada. 
     }
 
-    if(interStr == 'live-codeame'){
-    }
-        
-    if(interStr == 'regresar'){
-	
-	const coords = {x: th.camera.position.x,
-			y: th.camera.position.y,
-			z: th.camera.position.z} // Start at (0, 0) 
-
-	tween = new TWEEN.Tween(coords, false) // Create a new tween that modifies 'coords'.
-	    .to({x: 0, y: 0, z: 200}, 2000) // Move to (300, 200) in 1 second.
-	    .easing(TWEEN.Easing.Quadratic.InOut) // Use an easing function to make the animation smooth.
-	    .onUpdate(() => {
-		th.camera.position.z=coords.z;
-	    })  
-	    .onStart(() => {
-	// 	th.camera.remove(sphere44); 
-		// Pienso que onComplete está bien para eliminar objetos no utilizados
-		// Podría reproducir algún sonido 
-		document.getElementById("info").innerHTML = 'portada'; // cuando tween termine 
-	    })
-	//trambién hay onComplete
-	    .start() // Start the tween immediately.
-    }
 }
 
 function change_uvs( geometry, unitx, unity, offsetx, offsety ) {
@@ -584,12 +525,12 @@ function livecodeame(){
     // podrían tener un orden inicial y luego descomponerse 
 
     // malla de 2x2 ¿Podría ser de otra manera?
-    
+
     for(let i = 0; i < xgrid; i++){
 	for (let j = 0; j < ygrid; j++){
 
 	    //const geometry22 = new THREE.SphereGeometry(1, 3, 4 );
-	    const geometry22 = new THREE.BoxGeometry(6, 6, 3); 
+	    const geometry22 = new THREE.BoxGeometry(1.5, 1.5, 0.5); 
 	    change_uvs( geometry22, ux, uy, i, j );
 	    // podría no hacer referencia a hydra sino a otra cosa, por ejemplo podrían tener formas, colores y materiales distintos dependiendo del capítulo o del tipo de nota. 
 	    // materialslc = new THREE.MeshStandardMaterial( { color: 0x6a6a6a, roughness: 0.5, metalness:0.1 } );
@@ -602,18 +543,20 @@ function livecodeame(){
 	    var posX, posY, posZ;
 	    var theta1 = Math.random() * (Math.PI*2);
 	    var theta2 = Math.random() * (Math.PI*2); 
-
+	    
+	    
 	    posX = Math.cos(theta1) * Math.cos(theta2)*1;
 	    posY = Math.sin(theta1)*1;
 	    posZ = Math.cos(theta1) * Math.sin(theta2)*1;
-
+	    
 	    //posX = ( i - xgrid / 12 ) -6;
 	    //posY = ( j - ygrid / 5 ) -2.5;
 	    //posZ = (Math.random() * 1)-0.5;
 	    
-	    pX[cCount] = posX*(Math.random()*100);
-	    pY[cCount] = posY*(Math.random()*100);
-	    pZ[cCount] = posZ*(Math.random()*100); 
+	    pX[cCount] = cCount*0.5 - (markdown.length/4); 
+	    // pX[cCount] = posX*(Math.random()*100);
+	    pY[cCount] = posY*(Math.random()*40);
+	    pZ[cCount] = posZ*(Math.random()*40); 
 	    cubos2[cCount].position.x = pX[cCount]  ; 
 	    cubos2[cCount].position.y = pY[cCount] ;
 	    cubos2[cCount].position.z = pZ[cCount]  ;
@@ -625,8 +568,8 @@ function livecodeame(){
 	    group.add(cubos2[cCount]); 
 	    // th.scene.add( cubos2[cCount] );
 	    cCount++;
-	    
 	}
+
     }
 
     th.scene.add(group); 
@@ -642,13 +585,13 @@ function loadFont(){
 	// onLoad callback
 	function ( font ) {
 	    fuente = font;
-	    console.log(font);
+	    // console.log(font);
 	    fBool = true; 
     
 	})
 }
 
-function texto( mensaje= "TRES ESTUDIOS ABIERTOS TRES ESTUDIOS ABIERTOS TRES ESTUDIOS ABIERTOS TRES ESTUDIOS ABIERTOS TRES ESTUDIOS ABIERTOS TRES ESTUDIOS ABIERTOS\nESCRITURAS PERFORMÁTICAS AUDIOVISUALES E INVESTIGACIÓN CON JAVASCRIPT" ){
+function texto( mensaje ){
     //const materialT = new THREE.MeshStandardMaterial({color: 0xffffff, metalnenss: 0.8, roughness: 0.2, flatShading: true});
 
     const materialT = new THREE.MeshBasicMaterial({color: 0xffffff});
@@ -681,18 +624,19 @@ function saveNotes(){
     const TurndownService = require('turndown').default;	
     var turndownService = new TurndownService()
 
-    let notas = []; 
-    console.log(db.postdb); 
+    // aquí ya se leen las notas por fecha de modificación 
+    // console.log(db.postdb); 
+
     let contNota = 0;
 	
     for(let i = 0; i < db.postdb.length; i++){
 	if(db.postdb[i].length > 2){
-	    notas[contNota] = (db.postdb[i]);
+	    notas[contNota] = db.postdb[i];
 	    contNota++; 
 	}
     }
 
-    // console.log(notas); 
+    // console.log(notas.length); 
 
     for(let i = 0; i < notas.length; i++){
 	markdown[i] = turndownService.turndown(notas[i].toString());
@@ -700,7 +644,7 @@ function saveNotes(){
     }
 
     // queda pendiente eliminar indices 
-    // console.log(markdown); 
+    console.log(markdown.length); 
     
 }
 
@@ -712,7 +656,6 @@ function curve(positions){
     //Create a closed wavey loop
     curve1 = new THREE.CatmullRomCurve3( positions );
 
-    console.log(curve1); 
     curve1.closed = true; 
     
     const points = curve1.getPoints( 50 );
@@ -727,7 +670,7 @@ function curve(positions){
  
     curveObject.geometry.attributes.position.needsUpdate = true;
     
-    console.log(geometryCurve);
+    // console.log(geometryCurve);
     th.scene.add(curveObject); 
 }
  
